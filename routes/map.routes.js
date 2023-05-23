b6cb67a2dfa5e74dcb0cdad5f790d66fd08ef6c7
jujigithub <fallskywill@gmail.com>
2023-05-23 21:56:05 +0900

    fix:repositories 오타수정, map임시 커밋

--- routes/map.routes.js
+++ routes/map.routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
 // const authMiddleware = require("../middlewares/auth-middleware");
+const MapController = require("../controllers/map.controller");
+const mapController = new MapController();
+
+router.get("/", mapController.getMap);
 
 module.exports = router;
