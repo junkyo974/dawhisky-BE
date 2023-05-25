c0c198580451f8310acee16582a71b056b36433e
junkyo <124774799+junkyo974@users.noreply.github.com>
2023-05-25 11:03:26 +0900

    fix: 주석처리 부분 수정

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-// const likeRouter = require("./like.routes");
-// const mapRouter = require("./map.routes");
-// const mypageRouter = require("./mypage.routes");
-// const queRouter = require("./que.routes");
-// const reviewRouter = require("./review.routes");
-// const socketRouter = require("./socket.routes");
+const likeRouter = require("./like.routes");
+const mapRouter = require("./map.routes");
+const mypageRouter = require("./mypage.routes");
+const queRouter = require("./que.routes");
+const reviewRouter = require("./review.routes");
+const socketRouter = require("./socket.routes");
 const storeRouter = require("./store.routes");
 const userRouter = require("./user.routes");
-// const whiskyRouter = require("./whisky.routes");
+const whiskyRouter = require("./whisky.routes");
 const emailRouter = require("./email.routes");
 
 router.use("/auth", [userRouter, storeRouter, emailRouter]);
-// router.use("/mypage", mypageRouter);
-// router.use("/review", reviewRouter);
-// router.use("/que", queRouter);
-// router.use("/whisky", whiskyRouter);
-// router.use("/socket", socketRouter);
-// router.use("/map", mapRouter);
-// router.use("/like", likeRouter);
+router.use("/mypage", mypageRouter);
+router.use("/review", reviewRouter);
+router.use("/que", queRouter);
+router.use("/whisky", whiskyRouter);
+router.use("/socket", socketRouter);
+router.use("/map", mapRouter);
+router.use("/like", likeRouter);
 
 module.exports = router;
