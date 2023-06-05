cb4fa0462e72d546b0e2d908daccc770130e2350
Ju Jimin <123917608+jujigithub@users.noreply.github.com>
2023-06-05 11:17:05 +0900

    Update whisky.routes.js

--- routes/whisky.routes.js
+++ routes/whisky.routes.js
@@ -11,7 +11,7 @@ router.get("/search/:keyword", whiskyController.searchWhisky);
 
 // //위스키 전체조회
 // router.get("/", whiskyController.allWhisky);
-router.get("/", whiskyController.allWhisky);
+router.get("/", whiskyController.paginatedWhiskies);
 
 //위스키 상세조회
 router.get("/:whisky_id", whiskyController.whiskyDetail);
