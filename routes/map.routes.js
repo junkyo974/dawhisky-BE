ee398bbdde81e5fabadc301e3b0679c8ebe48f05
jihyunpark81 <jenny9681@naver.com>
2023-06-07 01:22:38 +0900

    feat: map get store #10

--- routes/map.routes.js
+++ routes/map.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const MapController = require("../controllers/map.controller");
 const mapController = new MapController();
 
-router.get("/", mapController.getMap);
+//스토어불러오기
+router.get("/store", mapController.getStore);
 
 module.exports = router;
