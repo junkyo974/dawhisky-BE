6d317a19375a34ee1c12e3e910721e75b3525779
jujigithub <fallskywill@gmail.com>
2023-06-07 01:33:11 +0900

    fix:hotfix

--- routes/map.routes.js
+++ routes/map.routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const MapController = require("../controllers/map.controller");
 const mapController = new MapController();
 
+//*주의* HTTPS/로드밸런서/라우트 헬스체크 코드입니다
+router.get("/", mapController.getMap);
+
 //스토어불러오기
 router.get("/store", mapController.getStore);
 
