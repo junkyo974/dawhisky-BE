d903357ef04cbedcc77a4c64b8f389052da35dc0
jujigithub <fallskywill@gmail.com>
2023-06-14 19:52:22 +0900

    feat:sockt good

--- modules/socket-handler.js
+++ modules/socket-handler.js
@@ -1,7 +1,24 @@
+const mysql = require("mysql");
 const QueService = require("../services/que.service");
 const queService = new QueService();
 
 const socketHandler = (io) => {
+  const connectionMysql = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  connectionMysql.connect((err) => {
+    if (err) {
+      console.error("MySQL connection failed:", err);
+      return;
+    }
+    console.log("Connected to MySQL");
+  });
+
+  // 소켓 연결
   io.on("connection", (socket) => {
     socket.on("enter", async (name, store_id) => {
       console.log(`${name}님이 ${store_id}에 접속하셨습니다.`);
@@ -9,31 +26,35 @@ const socketHandler = (io) => {
       socket.room = store_id;
       socket.join(store_id);
 
+      // MySQL 테이블 변경 사항 감지
+      const watchTableChanges = () => {
+        const query = "SELECT * FROM Ques";
+        connectionMysql.query(query, (err, rows) => {
+          if (err) {
+            console.error("Error in watching table changes:", err);
+            return;
+          }
+
+          // 변경된 데이터를 클라이언트에게 전송
+          io.to(socket.room).emit("재홍님 보이시나요", rows);
+          console.log("잘되고있지?");
+
+          // 일정 시간 간격으로 변경 사항 감지
+          setTimeout(watchTableChanges, 6000);
+        });
+      };
+
+      // 초기 테이블 데이터 감지 시작
+      watchTableChanges();
+
       try {
         const getQue = await queService.findAllQue(store_id);
 
         io.to(socket.room).emit("getQueData", getQue);
-        console.log("잘보내짐?", getQue);
+        console.log("잘 보내짐?", getQue);
       } catch (err) {
         console.log("점주 줄서기 socket 실패", err);
       }
-    });
-
-    socket.on("message", (data) => {
-      data.id = socket.id;
-      data.name = socket.name;
-      data.room = socket.room;
-      console.log("서버2", data);
-      socket.broadcast.to(socket.room).emit("update", data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log(`${socket.name} 님이 나가셨습니다.`);
-      io.to(socket.room).emit("update", {
-        type: "disconnect",
-        name: "POTATO MASTER",
-        message: `${socket.name} 님이 나가셨습니다.`,
-      });
     });
   });
 };
